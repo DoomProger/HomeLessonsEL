c824720bb57528389921ecf4f9d8a5e2d7c3feb9
DoomProger <doomwf@gmail.com>
2020-02-14 00:36:33 +0300

    Lesson_03_3

---
+++ Lesson3/Lesson_3_3.py
@@ -0,0 +1,17 @@
+'''
+Реализовать функцию my_func(), которая принимает
+три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
+'''
+
+
+def my_func(arg_1, arg_2, arg_3):
+    my_list = [arg_1, arg_2, arg_3]
+    min_arg = min(my_list)
+    return sum(my_list) - min_arg
+
+
+arg = []
+for el in range(0, 3):
+    arg.append(float(input('Введите число:')))
+
+print(f'Сумма наибольших двух аргументов из {arg} это {my_func(arg[0], arg[1], arg[2])}')
