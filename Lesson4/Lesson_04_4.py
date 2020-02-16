c57449f4c9225d2c4d28b684fa2ad53741ee8d38
DoomProger <doomwf@gmail.com>
2020-02-16 20:03:48 +0300

    Lesson_04_4

--- Lesson4/Lesson_04_4.py
+++ Lesson4/Lesson_04_4.py
@@ -4,3 +4,17 @@
 Элементы вывести в порядке их следования в исходном списке.
 Для выполнения задания обязательно использовать генератор.
 '''
+
+import random
+
+new_list = [el*random.randrange(-11, 11) for el in range(20)]
+print(new_list)
+
+for i, v in enumerate(new_list):
+    if new_list.count(v) > 1:
+        srez = new_list.index(v) + 1
+        indx_list = new_list[srez:].index(v) + srez
+        new_list.pop(indx_list)
+
+print()
+print(new_list)
