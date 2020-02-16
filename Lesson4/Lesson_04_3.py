bee88bf8268589bbb503a29f88bc2a90faaab263
DoomProger <doomwf@gmail.com>
2020-02-16 16:58:25 +0300

    Lesson_04_3 one line

--- Lesson4/Lesson_04_3.py
+++ Lesson4/Lesson_04_3.py
@@ -4,5 +4,4 @@
 Подсказка: использовать функцию range() и генератор.
 '''
 
-new_list = [el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]
-print(new_list)
+print([el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0])
